4ad19dd6ccafa38982fe18932d3324dcd999caa7
Lilly <128197764+Lillinator@users.noreply.github.com>
2023-09-10 07:12:04 -0700

    Update topic-list-author.js

--- javascripts/discourse/components/topic-list-author.js
+++ javascripts/discourse/components/topic-list-author.js
@@ -1,44 +1,37 @@
+import Category from "discourse/models/category";
 import Component from "@ember/component";
-import { equal } from "@ember/object/computed";
 import discourseComputed from "discourse-common/utils/decorators";
 
-const rawCategoryTopicAuthor = settings.apply_to_these_categories_only.split("|");
-
 export default Component.extend({
-  
-  
-  
-  
-  
-  
-  
-  noCategoryStyle: equal("siteSettings.category_style", "none"),
-  boxStyle: equal("siteSettings.desktop_category_page_style", "categories_boxes"),
+  isBlogTopic: false,
+  imageURL: null,
+  @discourseComputed("topic.category_id", "topic.tags")
+  isBlogTopic(categoryId, topicTags) {
+    let hasCategory = false;
+    let hasTag = false;
 
-  @discourseComputed()
-  preview() {
-    const previewData = [];
-    const loggedInUser = this.currentUser;
-    const loggedInUserGroup = loggedInUser ? loggedInUser.groups.map(g => g.name) : [];
-    const isStaff = loggedInUser ? loggedInUser.staff : false;
-    const categorySlug = this.args.category.slug;
+    if(settings.no_images) {
+      return false;
+    }
 
-    rawCategoryPreviews.forEach(rawPreview => {
-      const previewPart = rawPreview.split("~");
-      const permittedGroup = previewPart[4] ? previewPart[4].split(",") : [];
-      const hasCategoryAccess = loggedInUserGroup.some(g => permittedGroup.indexOf(g) > -1);
-      const shouldRender = !loggedInUser || isStaff || !hasCategoryAccess;
+    if(categoryId) {
+      const allowedCategories = settings.blog_category.split("|");
+      const currentCategory = Category.findById(categoryId);
+      const parentCategorySlug = currentCategory.parentCategory ? `${currentCategory.parentCategory.slug}-` : "";
+      hasCategory = allowedCategories.some((c) => c.trim() === `${parentCategorySlug}${currentCategory.slug}`);
+    }
 
-      if (shouldRender && categorySlug === previewPart[0]) {
-        previewData.push({
-          title: previewPart[1],
-          description: previewPart[2],
-          href: previewPart[3],
-          className: `above-${categorySlug}`,
-          color: settings.border_color
-        });
-      }
-    });
+    if(topicTags) {
+      const allowedTags = settings.blog_tag.split("|");
+      hasTag = allowedTags.some((tag) => topicTags.includes(tag));
+    }
 
-    return previewData;
-  }
+    return hasCategory || hasTag;
+  },
+  @discourseComputed("topic.thumbnails")
+  imageURL(thumbnails) {
+    if(thumbnails) {
+      return thumbnails[0].url;
+    }
+  },
+});
