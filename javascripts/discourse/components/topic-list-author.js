df0a1ba75430f17fd09a2a66b38b90ef972969d9
Lilly <128197764+Lillinator@users.noreply.github.com>
2023-09-10 08:06:26 -0700

    Update topic-list-author.js

--- javascripts/discourse/components/topic-list-author.js
+++ javascripts/discourse/components/topic-list-author.js
@@ -3,19 +3,11 @@ import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
-  isBlogTopic: false,
-  imageURL: null,
-  @discourseComputed("topic.category_id", "topic.tags")
-  isBlogTopic(categoryId, topicTags) {
-    let hasCategory = false;
-    let hasTag = false;
-
-    if(settings.no_images) {
-      return false;
-    }
+  
+  
 
     if(categoryId) {
-      const allowedCategories = settings.blog_category.split("|");
+      const allowedCategories = settings.apply_to_these_categories_only.split("|");
       const currentCategory = Category.findById(categoryId);
       const parentCategorySlug = currentCategory.parentCategory ? `${currentCategory.parentCategory.slug}-` : "";
       hasCategory = allowedCategories.some((c) => c.trim() === `${parentCategorySlug}${currentCategory.slug}`);
